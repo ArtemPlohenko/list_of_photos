2cf77b38af9158638c3e7065bee93ddbd58fe30e
Artem Plokhenko <artemplohenko@gmail.com>
2022-11-01 21:29:24 +0200

    fixed

--- src/AlbumPage.jsx
+++ src/AlbumPage.jsx
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react";
-import {useParams, Link} from "react-router-dom";
-import {Button, ListGroup, Form} from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { Button, ListGroup, Form } from "react-bootstrap";
 
 const AlbumPage = (props) => {
-  const {id} = useParams();
+  const { id } = useParams();
   const size = 10;
   const [offset, setOffset] = useState(0);
   const [photos, setPhotos] = useState([]);
@@ -22,15 +22,14 @@ const AlbumPage = (props) => {
   }, [offset, search]);
 
   return (
-    
     <div className="wrapper">
-      <Link
-        className="mb-15"
-        to="/albums">
-          Back
+      <Link className="mb-15" to="/albums">
+        Back
       </Link>
       <div className="btns-block flex-group mb-15">
-        <Button onClick={() => setOffset(Math.max(offset -size, 0))}>prev</Button>
+        <Button onClick={() => setOffset(Math.max(offset - size, 0))}>
+          prev
+        </Button>
         {offset / size}
         <Button onClick={() => setOffset(offset + size)}>next</Button>
       </div>
@@ -41,16 +40,16 @@ const AlbumPage = (props) => {
             type="text"
             placeholder="Search..."
             onChange={(event) => {
-            setSearch(event.target.value);
+              setSearch(event.target.value);
             }}
-           />
+          />
         </Form.Group>
       </Form>
       <div className="list-group-holder">
         <ListGroup>
           {photos.map((photo) => (
             <ListGroup.Item key={photo.id}>
-              <h2 className="title-h2">{photo.`title`}</h2>
+              <h2 className="title-h2">{photo.title}</h2>
               <img alt={props.title} src={photo.thumbnailUrl} />
             </ListGroup.Item>
           ))}
